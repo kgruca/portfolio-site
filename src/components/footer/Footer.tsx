6228cbba2b9e40c90c74dcfb57c65971eb9b02e0
kgruca <kgruca21@gmail.com>
2023-08-02 22:00:47 -0700

    css added to layout, footer, navbar

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./footer.module.css";
 
 const Footer = () => (
-  <div>
+  <div className={styles.container}>
     <div>&copy;2023 Gruca&Maso Productions, Inc. All rights reseved.</div>
     <div></div>
   </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import styles from "./navbar.module.css";
 
 const links = [
   {
@@ -35,14 +37,24 @@ const links = [
 ];
 
 const Navbar = () => (
-  <div>
-    <Link href="/">g&m</Link>
-    <div>
+  <div className={styles.container}>
+    <Link href="/" className={styles.logo}>
+      g&m
+    </Link>
+    <div className={styles.links}>
       {links.map((link) => (
-        <Link key={link.id} href={link.url}>
+        <Link key={link.id} href={link.url} className={styles.link}>
           {link.title}
         </Link>
       ))}
+      <button
+        className={styles.logout}
+        onClick={() => {
+          console.log("logged out");
+        }}
+      >
+        Logout
+      </button>
     </div>
   </div>
 );
