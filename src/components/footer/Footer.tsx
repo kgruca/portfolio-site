d1d2df19b72909f765877ced801333a624258123
kgruca <kgruca21@gmail.com>
2023-08-02 18:07:21 -0700

    layout.tsx updated with new navbar and footer components

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <div>
+    <div>&copy;2023 Gruca&Maso Productions, Inc. All rights reseved.</div>
+    <div></div>
+  </div>
+);
+
+export default Footer;
