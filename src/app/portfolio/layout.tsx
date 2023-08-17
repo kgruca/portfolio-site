6c2bb576e824aec46cabf22d858fb4eba23d88da
kgruca <kgruca21@gmail.com>
2023-08-16 22:12:57 -0700

    layout created for portfolio

---
+++ src/app/portfolio/layout.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const layout = () => (
+  <div>
+    <h1 className={styles.mainTitle}>Our Works</h1>
+  </div>
+);
+
+export default layout;
