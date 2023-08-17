849db47599fc48edb759a3d1a6f3a42fbe210e37
kgruca <kgruca21@gmail.com>
2023-08-16 19:51:17 -0700

    contact page created

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,33 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Button from "@/components/button/Button";
 
-const Contact = () => <div className={styles.contact}>Contact</div>;
+const Contact = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
+    <div className={styles.content}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="Contact Us"
+          fill
+          className={styles.img}
+        />
+      </div>
+      <form className={styles.form}>
+        <input type="text" placeholder="name" className={styles.input} />
+        <input type="text" placeholder="email" className={styles.input} />
+        <textarea
+          className={styles.textArea}
+          cols={30}
+          rows={10}
+          placeholder="message"
+        ></textarea>
+        <Button url="#" text="Send" />
+      </form>
+    </div>
+  </div>
+);
 
 export default Contact;
