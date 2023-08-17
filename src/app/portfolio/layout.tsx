caa02129cfcd04e852ca9620fa6dc3b6470ae82a
kgruca <kgruca21@gmail.com>
2023-08-16 23:14:26 -0700

    styling and images added to portfolio page

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./page.module.css";
 
-const layout = () => (
+const layout = ({ children }: { children: React.ReactNode }) => (
   <div>
     <h1 className={styles.mainTitle}>Our Works</h1>
+    {children}
   </div>
 );
 
