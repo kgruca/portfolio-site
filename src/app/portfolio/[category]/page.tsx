7e1f510c4a2a1155e27b151f63c542c5eb80faf6
kgruca <kgruca21@gmail.com>
2023-08-18 21:59:34 -0700

    stylizing categories pages for portfolio

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,11 +1,30 @@
 import React from "react";
-import styles from "..page.module.css";
+import styles from "./page.module.css";
+import Button from "@/components/button/Button";
+import Image from "next/image";
 
 const Category = ({ params }) => {
   console.log(params);
   return (
     <>
-      <h1>Category</h1>
+      <div className={styles.container}>
+        <h1 className={styles.catTitle}>{params.category}</h1>
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Test</h1>
+            <p className={styles.desc}>Description</p>
+            <Button text="See More" url="#" />
+          </div>
+          <div className={styles.imgContainer}>
+            <Image
+              className={styles.img}
+              fill
+              src="/illustration2.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
