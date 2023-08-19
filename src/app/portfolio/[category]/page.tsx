d7ae36f13b222336a6033de628eb089fa5620f7a
kgruca <kgruca21@gmail.com>
2023-08-19 16:24:28 -0700

    portfolio page layout done

--- src/app/portfolio/[category]/page.tsx
+++ src/app/portfolio/[category]/page.tsx
@@ -1,14 +1,33 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/button/Button";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
-const Category = ({ params }) => {
-  console.log(params);
+const getPosition = (str: string, subStr: string, pos: number) => {
+  let length = str.length,
+    i = -1;
+
+  while (pos-- && i++ < length) {
+    i = str.indexOf(subStr, i);
+    if (i < 0) break;
+  }
+  return i;
+};
+
+const Category = () => {
+  const pathName = usePathname();
+  const startPosition = getPosition(pathName, "/", 2) + 1;
+
+  const categoryName = pathName.slice(startPosition);
+
+  console.log(categoryName);
+
   return (
     <>
       <div className={styles.container}>
-        <h1 className={styles.catTitle}>{params.category}</h1>
+        <h1 className={styles.catTitle}>{categoryName}</h1>
         <div className={styles.item}>
           <div className={styles.content}>
             <h1 className={styles.title}>Test</h1>
