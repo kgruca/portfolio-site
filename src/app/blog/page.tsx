e1df38e6b1c65064016cee56aa30847b089d3af0
kgruca <kgruca21@gmail.com>
2023-08-19 17:00:43 -0700

    skeleton created for blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import fatKitty from "/public/fatKitty.png";
 
-const Blog = () => {
-  return <div className={styles.blog}>Blog</div>;
-};
+const Blog = () => (
+  <div className={styles.container}>
+    <Link href="/testID" className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img}
+          src={fatKitty}
+          alt=""
+          height={250}
+          width={400}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Test</h1>
+        <p className={styles.desc}>Description</p>
+      </div>
+    </Link>
+  </div>
+);
 
 export default Blog;
